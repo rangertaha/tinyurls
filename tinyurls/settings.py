cf7e83e7e10180686d99ed5be4f61534eb8e9d55
Tom Lee <tom@cybint.io>
2018-08-03 13:41:38 -0400

    Default settings

---
+++ tinyurls/settings.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Default Settings
+
+"""
+
+
+HTTP_PORT = 7777
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'main': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    },
+}
+
+DATABASE = 'sqlite:///db.sqlite3'
